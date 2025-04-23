4eaaac3c0f70505427b58235a54e54466cc25faf
Nazmul <nazmulislamfsd@gmail.com>
2025-04-23 22:10:18 +0600

    module 7

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
-from tasks.models import Employee, Task
+from tasks.models import Employee, Task, Task_detail, Project
+from datetime import date
+from django.db.models import Q, Count, Max, Min, Sum, Avg
 
 # Create your views here.
 
@@ -61,6 +63,23 @@ def create_task(request):
     return render(request, "taskForm.html",context)
 
 def view_task(request):
-    tasks = Task.objects.all()
+    # tasks = Task.objects.all()
+    # tasks = Task.objects.filter(status="PENDING")
+    # tasks = Task.objects.filter(due_date=date.today())
+    # tasks = Task_detail.objects.exclude(priority="L")
+    # tasks = Task.objects.filter(id__gt=5)
+    # tasks = Task.objects.filter(title__contains="paper")
+    # tasks = Task_detail.objects.select_related('task').all()
+    # tasks = Task.objects.filter(description__icontains = "c", status = "PENDING") '''AND Operation'''
+    # tasks = Task.objects.filter(Q(status="PENDING") | Q(status="IN_PROGRESS"))
+    # tasks = Task.objects.select_related('project').all()
+    # tasks = Task.objects.select_related('details').all()
+    # tasks = Task_detail.objects.select_related('task').all()
+    # tasks = Task_detail.objects.select_related('task').all()
+    # projects = Project.objects.prefetch_related('allTask').all()
+    # countTask = Task.objects.aggregate(num_count= Count("id"))
+
+    projects = Project.objects.annotate(num_task = Count("allTask")).order_by("id")
     
-    return render(request,"view_task.html",{"tasks":tasks})
+
+    return render(request,"view_task.html",{"projects":projects})
